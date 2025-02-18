ce8b12c70b9c0aba1466f0dd1a96e0e2dc9ce778
Marcus Ceradini <mnceradini@my.waketech.edu>
2025-02-18 14:01:52 -0500

    Project started

---
+++ script.js
@@ -0,0 +1,12 @@
+const image1 = document.getElementById("image1")
+const image2 = document.getElementById("image2")
+const image3 = document.getElementById("image3")
+const b1 = document.getElementById("b1")
+const b2 = document.getElementById("b2")
+const b3 = document.getElementById("b3")
+//change image => image1.sec = "new image"
+
+b1.addEventListener('click', () => {
+    console.log("hi")
+})
+
