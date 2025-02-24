c71fa1669e2f7772a01a7083734217b8c28eb079
MarcusCeradini <mnceradini@my.waketech.edu>
2025-02-24 09:45:54 -0500

    fixed crime scene image

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function askchristopher() {
 function crimescene() {
     header.textContent = "Investigation Arc"
     text.textContent = "You decide to go back to the crime scene and you find the culpret's armour lying there. He seems to have pilfered Karim's armour from his dead body"
-    image1.src = "chrisbasearmour.png"
+    image1.src = "chrisbasearmor.png"
     image1.style.display = "flex"
     b1.textContent = "Question gunpowder farm people"
     b1.style.display = "flex"
